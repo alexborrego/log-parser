9d3456582be1accc5286992322d40de8a7f5e4cc
Alex Borrego <aborrego@catalyte.io>
2022-04-16 19:50:33 -0600

    add maxind library to pull location data

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const fs = require("fs");
 const AccessLogParser = require('outlawdesigns.io.accesslogparser');
 const logPath = 'sample.log';
+const maxmind = require('maxmind');
 
-let data = fs.readFileSync(logPath).toString().split("\n");
+let log = fs.readFileSync(logPath).toString().split("\n");
 
-data.forEach((line)=>{
-    console.log(AccessLogParser.parseLine(line));
+
+
+maxmind.open('src/database/GeoLite2-City.mmdb').then((lookup) => {
+    log.forEach((line)=>{
+
+        console.log(AccessLogParser.parseLine(line));
+    });
+    console.log(lookup.get('66.6.44.4'));
 });
