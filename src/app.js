004cc68e06b343c4be04b6f9c064b63cf8b0dd60
Alex Borrego <aborrego@catalyte.io>
2022-04-17 00:20:59 -0600

    finish building output array, add csv conversion

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 const fs = require("fs");
 const AccessLogParser = require('outlawdesigns.io.accesslogparser');
-const logPath = 'sample.log';
+const deviceParser = require('ua-parser-js');
+const { Parser } = require('json2csv');
 const maxmind = require('maxmind');
+const csvParser = new Parser(opts);
+
+const fields = ['ip_address','responseCode','requestDate','requestMethod','query','referrer','responseSize','browser_name','browser_version','operating_system_name','operating_system_version','is_mobile', 'country', 'state', 'device']
+const opts = { fields };
+const logPath = 'sample.log';
+
 let testLog = fs.readFileSync(logPath).toString().split("\n");
 let outputArr = new Array();
 
 maxmind.open('src/database/GeoLite2-City.mmdb').then((lookup) => {
-    let geoData, logLine;
+    let geoData, logLine, device;
     testLog.forEach((line)=>{
         if(line !== ''){
-            logLine = AccessLogParser.parseLine(line)
-            geoData = lookup.get(logLine.ip_address)
+            logLine = AccessLogParser.parseLine(line);
+            device = deviceParser(line).device.type;
+            geoData = lookup.get(logLine.ip_address);
             if (typeof geoData.country === "undefined") {
                 logLine.country = "Unknown";
             }
@@ -23,12 +31,21 @@ maxmind.open('src/database/GeoLite2-City.mmdb').then((lookup) => {
             else {
                 logLine.state = geoData.subdivisions[0].names.en
             }
+            if (typeof device === 'undefined') {
+                logLine.device = 'desktop';
+            }
+            else {
+                logLine.device = device;
+            }
             outputArr.push(logLine);
         }
     });
+}, () => {
+    console.log("An unexpected error has occured. Please try again.");
 })
 .then(() => {
-    console.log(outputArr);
+    let csv = csvParser.parse(outputArr);
+    console.log(csv);
 })
 
 // .then((geoData) => {
