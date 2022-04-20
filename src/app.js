5cd27704e22ed9403bb0abbbc024e0250ac81ba2
Alex Borrego <aborrego@catalyte.io>
2022-04-19 23:42:54 -0600

    begin working on testing and fixing error handling within app

--- src/app.js
+++ src/app.js
@@ -8,8 +8,14 @@ const fields = ['ip_address','responseCode','requestDate','requestMethod','query
 const opts = { fields };
 const csvParser = new Parser(opts);
 
-const args = process.argv.slice(2);
-let inputLog = fs.readFileSync(args[0]).toString().split('\n');
+/**
+ * Reads user-entered input log parameter
+ * @returns Input Log
+ */
+const openLog = () => {
+    const args = process.argv.slice(2);
+    return fs.readFileSync(args[0]).toString().split('\n');
+}
 
 /**
  * Iterates over each entry in the log file and
@@ -20,11 +26,16 @@ let inputLog = fs.readFileSync(args[0]).toString().split('\n');
 const generateOutput = (lookup) => {
     let geoData, logLine, device;
     let outputArr = new Array();
-        inputLog.forEach((line)=>{
+        openLog().forEach((line)=>{
             if(line !== ''){
                 logLine = AccessLogParser.parseLine(line);
                 device = deviceParser(line).device.type;
-                geoData = lookup.get(logLine.ip_address);
+                try {
+                    geoData = lookup.get(logLine.ip_address);
+                }
+                catch(e){
+                    throw e;
+                }
                 logLine.country = (typeof geoData.country === 'undefined') ? 'Unknown' : geoData.country.names.en;
                 logLine.state = (typeof geoData.subdivisions === 'undefined') ? 'Unknown' : geoData.subdivisions[0].names.en;
                 logLine.device = (typeof device === 'undefined') ? 'desktop' : device;
@@ -35,11 +46,19 @@ const generateOutput = (lookup) => {
 }
 
 /**
- * Converts JSON to CSV and writes it to a file.
- * @param {JSON} input 
+ * Converts the JSON array to a CSV string.
+ * @param {JSON} json 
+ * @returns CSV string
  */
-const createFile = (JSON) => {
-    let csv = csvParser.parse(JSON);
+const convertCSV = (json) => {
+    return csvParser.parse(json);
+}
+
+/**
+ * Creates the file from csv.
+ * @param {string} csv 
+ */
+const createFile = (csv) => {
     fs.writeFile('Access Log.csv', csv, (err) => {
         console.log((err === null) ? 'Success! Please check the folder.' : err);
     }); 
@@ -49,6 +68,26 @@ const createFile = (JSON) => {
  * Calls the above functions to read from the database,
  * build the JSON object, convert to CSV, and output.
  */
-maxmind.open('src/database/GeoLite2-City.mmdb')
-.then((lookup) => generateOutput(lookup))
-.then((JSON) => createFile(JSON)).catch(() => console.log('Please provide a valid path to the log file.'));
+const main = async () => {
+    try {
+    maxmind.open('src/database/GeoLite2-City.mmdb')
+    .then((lookup) => generateOutput(lookup))
+        // .catch(() => {
+        //     console.log('Please provide a valid path to the log file, or check the format of your log.')
+        //     throw 'a';
+        // })
+    .then((json) => convertCSV(json))
+    .then((csv) => createFile(csv))
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+main();
+
+
+exports.openLog = openLog;
+exports.generateOutput = generateOutput;
+exports.convertCSV = convertCSV;
+exports.createFile = createFile
