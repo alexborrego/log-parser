7d6bd7ff138714a2882946f5e02f3c6abf01a06d
Alex Borrego <aborrego@catalyte.io>
2022-04-19 21:35:22 -0600

    finish up logic to create and output a CSV

--- src/app.js
+++ src/app.js
@@ -1,70 +1,54 @@
-const fs = require("fs");
+const fs = require('fs');
 const AccessLogParser = require('outlawdesigns.io.accesslogparser');
 const deviceParser = require('ua-parser-js');
 const { Parser } = require('json2csv');
 const maxmind = require('maxmind');
-const csvParser = new Parser(opts);
 
 const fields = ['ip_address','responseCode','requestDate','requestMethod','query','referrer','responseSize','browser_name','browser_version','operating_system_name','operating_system_version','is_mobile', 'country', 'state', 'device']
 const opts = { fields };
-const logPath = 'sample.log';
+const csvParser = new Parser(opts);
 
-let testLog = fs.readFileSync(logPath).toString().split("\n");
-let outputArr = new Array();
+const args = process.argv.slice(2);
+let inputLog = fs.readFileSync(args[0]).toString().split('\n');
 
-maxmind.open('src/database/GeoLite2-City.mmdb').then((lookup) => {
+/**
+ * Iterates over each entry in the log file and
+ * looks up geographic data in the Maxmind database.
+ * @param {Reader} lookup 
+ * @returns JSON object of log file
+ */
+const generateOutput = (lookup) => {
     let geoData, logLine, device;
-    testLog.forEach((line)=>{
-        if(line !== ''){
-            logLine = AccessLogParser.parseLine(line);
-            device = deviceParser(line).device.type;
-            geoData = lookup.get(logLine.ip_address);
-            if (typeof geoData.country === "undefined") {
-                logLine.country = "Unknown";
+    let outputArr = new Array();
+        inputLog.forEach((line)=>{
+            if(line !== ''){
+                logLine = AccessLogParser.parseLine(line);
+                device = deviceParser(line).device.type;
+                geoData = lookup.get(logLine.ip_address);
+                logLine.country = (typeof geoData.country === 'undefined') ? 'Unknown' : geoData.country.names.en;
+                logLine.state = (typeof geoData.subdivisions === 'undefined') ? 'Unknown' : geoData.subdivisions[0].names.en;
+                logLine.device = (typeof device === 'undefined') ? 'desktop' : device;
+                outputArr.push(logLine);
             }
-            else{
-                logLine.country = geoData.country.names.en;
-            }
-            if (typeof geoData.subdivisions === "undefined") {
-                logLine.state = "Unknown";
-            }
-            else {
-                logLine.state = geoData.subdivisions[0].names.en
-            }
-            if (typeof device === 'undefined') {
-                logLine.device = 'desktop';
-            }
-            else {
-                logLine.device = device;
-            }
-            outputArr.push(logLine);
-        }
-    });
-}, () => {
-    console.log("An unexpected error has occured. Please try again.");
-})
-.then(() => {
-    let csv = csvParser.parse(outputArr);
-    console.log(csv);
-})
+        });
+    return outputArr;
+}
 
-// .then((geoData) => {
-            
-//     if (typeof geoData.country === "undefined") {
-//         logLine.country = "Unknown";
-//     }
-//     else{
-//         logLine.country = geoData.country.names.en;
-//     }
-//     if (typeof geoData.subdivisions === "undefined") {
-//         logLine.state = "Unknown";
-//     }
-//     else {
-//         logLine.state = geoData.subdivisions[0].names.en
-//     }
-//     outputArr.push(logLine);
+/**
+ * Converts JSON to CSV and writes it to a file.
+ * @param {JSON} input 
+ */
+const createFile = (JSON) => {
+    let csv = csvParser.parse(JSON);
+    fs.writeFile('Access Log.csv', csv, (err) => {
+        console.log((err === null) ? 'Success! Please check the folder.' : err);
+    }); 
+}
 
-// })
-// .then(() => {
-// console.log(outputArr);
-// })
+/**
+ * Calls the above functions to read from the database,
+ * build the JSON object, convert to CSV, and output.
+ */
+maxmind.open('src/database/GeoLite2-City.mmdb')
+.then((lookup) => generateOutput(lookup))
+.then((JSON) => createFile(JSON)).catch(() => console.log('Please provide a valid path to the log file.'));
